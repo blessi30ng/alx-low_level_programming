a5976b2d0d20e3b6bf4d581ab283fbe1f577953b
root <blessi30ng>
2023-09-11 06:59:01 -0700

    task 6

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,13 @@
+#include "dog.h"
+#include <stdio.h>
+
+/**
+ * new_dog - prints new dog
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of dog
+ */
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	if (
