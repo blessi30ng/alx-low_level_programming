c7d98b34bc163a324c292bf80688dc441754b3c4
blessi30ng <blessingchidewu3@gmail.com>
2023-10-27 06:28:23 -0700

    yelp

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - main funct
+ * @argc: arg counter
+ * @argv: arg vector
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	(void) argv;
+
+	printf("%d\n", argc - 1);
+	return (0);
+}
