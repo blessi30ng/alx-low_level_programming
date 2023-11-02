afbe394c95ab3eca414c27e555005c96761b5e10
blessi30ng <blessingchidewu3@gmail.com>
2023-11-02 08:14:16 -0700

    yoh

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * string_nconcat - concatenates string
+ * @s2: s2
+ * @s1: s1
+ * @n: no of bytes
+ * Return: concatenated string
+
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+
